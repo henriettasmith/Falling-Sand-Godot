f545e3c5cc98582b2ee23b40caebe67f03fe0c5d
Henrietta Smith <henrietta@henrietta.dev>
2019-08-06 22:03:27 -0700

    Small Changes

--- Main.cs
+++ Main.cs
@@ -3,5 +3,18 @@ using System;
 
 public class Main : TileMap
 {
-    
+    public void SetParticle(int id)
+    {
+        GD.Print(id);
+    }
+
+    public void SetBrushSize(int size)
+    {
+        GD.Print(size);
+    }
+
+    public void TogglePause(bool paused)
+    {
+        GD.Print(paused);
+    }
 }
